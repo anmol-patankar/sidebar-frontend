517b7d6ef0c2b275991bdfd0068ba9d2c158bf6f
Anmol <anmol.patankar@coditas.com>
2024-06-28 11:09:44 +0530

    fixed scripts

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const iconContainers = document.querySelectorAll('.sdicon');
     iconContainers.forEach(container => {
-        container.classList.remove('sdicon-active'); // Ensure all icons start without the 'active' class
+        container.classList.remove('sdicon-active'); 
         container.addEventListener('click', () => {
-            // Remove the 'active' class from all icon containers
             iconContainers.forEach(icon => icon.classList.remove('sdicon-active'));
-            // Add the 'active' class to the clicked icon container
             container.classList.add('sdicon-active');
         });
     });
