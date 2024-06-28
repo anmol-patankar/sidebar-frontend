3a40e4235543f254a73e857e915b28bad1ae979d
Jaykumar Pokar <jaykumar.pokar@coditas.com>
2024-06-28 11:38:31 +0530

    Icon folder - added and anchor tag added

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const iconContainers = document.querySelectorAll('.sdicon');
     iconContainers.forEach(container => {
-        container.classList.remove('sdicon-active'); 
+        container.classList.remove('sdicon-active');
         container.addEventListener('click', () => {
             iconContainers.forEach(icon => icon.classList.remove('sdicon-active'));
             container.classList.add('sdicon-active');
